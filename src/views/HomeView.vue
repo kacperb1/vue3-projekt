571f1715033c8fd2141296c2a99cf62a664b65d2
kacperb1 <70506666+kacperb1@users.noreply.github.com>
2023-05-09 17:00:16 +0200

    New Homepage w/ Composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,45 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
-    <font-awesome-icon icon="fa-solid fa-xmark" />
+  <div class="home-page">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">Click me!</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const title = 'Welcome to the Home Page';
+const message = ref('This is a simple Vue 3 component using TypeScript and Composition API.');
+
+function changeMessage() {
+  message.value = 'You have clicked the button!';
+}
+</script>
+
+<style scoped>
+.home-page {
+  text-align: center;
+  padding: 20px;
+}
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+
+button {
+  background-color: #42b983;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 0.5rem 1rem;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #2c9e6f;
+}
+</style>
